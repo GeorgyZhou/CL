349fab00441597327dc3163636e6761123691d7f
GeorgyZhou <michaelchouqj@gmail.com>
2018-03-13 23:02:06 -0700

    understand the SI logic more

--- models/model_SI.py
+++ models/model_SI.py
@@ -8,6 +8,62 @@
 # Centre, Sydney, Australia: PMLR), pp. 3987-3995.
 # http://proceedings.mlr.press/v70/zenke17a.html
 
+import numpy as np
+
+import tensorflow as tf
+
 class SIModel:
   def __init__(self, *args, **kwargs):
-    
+    pass
+
+  def fit(self, train_type, task_dict, *args, **kwargs):
+    pass
+
+  def evaluate(self, *args, **kwargs):
+    pass
+
+  def summary(self):
+    pass
+
+  def predict(self, *args, **kwargs):
+    pass
+
+  def save_weights(self, file_prefix, overwrite=True):
+    pass
+
+  def load_weights(self, file_prefix):
+    pass
+
+  def __get_numvals_list(self, key='omega'):
+    """Returns list of numerical values such as for instance omegas in reproducible order.
+
+    Args:
+      key: key for values to be extracted.
+    Returns:
+      A list containing values for that key.
+    """
+    variables = self.vars[key]
+    numvals = []
+    for p in self.weights:
+      numval = K.get_value(tf.reshape(variables[p],(-1,)))
+      numvals.append(numval)
+    return numvals
+
+  def __quadratic_regularizer(self, weights, vars, norm=2):
+    """Compute the regularization term.
+
+    Args:
+      weights: list of Variables
+      vars: dict from variable name to dictionary containing the variables.
+            Each set of variables is stored as a dictionary mapping from weights to variables.
+            For example, vars['grads'][w] would retreive the 'grads' variable for weight w
+      norm: power for the norm of the (weights - consolidated weight)
+    Returns:
+      scalar Tensor regularization term
+    """
+    reg = 0.0
+    for w in weights:
+      reg += tf.reduce_sum(vars['omega'][w] * (w - vars['cweights'][]))
+    return reg
+
+CLModel = SIModel

d25e4adc79f17aeaec3729e49b16f8caf88063cf
GeorgyZhou <michaelchouqj@gmail.com>
2018-03-16 00:36:30 -0700

    extract weight changes

--- models/model_SI.py
+++ models/model_SI.py
@@ -14,6 +14,7 @@ import tensorflow as tf
 
 class SIModel:
   def __init__(self, *args, **kwargs):
+
     pass
 
   def fit(self, train_type, task_dict, *args, **kwargs):
@@ -34,9 +35,7 @@ class SIModel:
   def load_weights(self, file_prefix):
     pass
 
-
-
-  def __get_values_list(self, key='omega'):
+  def _get_values_list(self, key='omega'):
     """Returns list of numerical values such as for instance omegas in reproducible order.
 
     Args:
@@ -52,7 +51,40 @@ class SIModel:
     return values
 
   @staticmethod
-  def __quadratic_regularizer(weights, vars, norm=2):
+  def _extract_weight_changes(weights, update_ops):
+    """Given a list of weights and Assign ops, identify the change in weights.
+
+    Args:
+        weights: list of Variables
+        update_ops: list of Assign ops, typically computed using Keras' opt.get_updates()
+
+    Returns:
+        list of Tensors containing the weight update for each variable
+    """
+    name_to_var = {v.name: v.value() for v in weights}
+    weight_update_ops = list(filter(lambda x: x.op.inputs[0].name in name_to_var, update_ops))
+    nonweight_update_ops = list(filter(lambda x: x.op.inputs[0].name not in name_to_var, update_ops))
+    # Make sure that all the weight update ops are Assign ops
+    for weight in weight_update_ops:
+      if weight.op.type != 'Assign':
+        raise ValueError('Update op for weight %s is not of type Assign.' % weight.op.inputs[0].name)
+    weight_changes = [(new_w.op.inputs[1] - name_to_var[new_w.op.inputs[0].name]) for new_w, old_w in
+                      zip(weight_update_ops, weights)]
+    # Recreate the update ops, ensuring that we compute the weight changes before updating the weights
+    with tf.control_dependencies(weight_changes):
+      new_weight_update_ops = [tf.assign(new_w.op.inputs[0], new_w.op.inputs[1]) for new_w in weight_update_ops]
+    return weight_changes, tf.group(*(nonweight_update_ops + new_weight_update_ops))
+
+  @staticmethod
+  def _compute_updates(opt, loss, weights):
+    update_ops = opt.get_updates(weights, [], loss)
+    deltas, new_update_op = SIModel._extract_weight_changes(weights, update_ops)
+    grads = tf.gradients(loss, weights)
+    # Make sure  that deltas are computed _before_ the weight is updated
+    return new_update_op, grads, deltas
+
+  @staticmethod
+  def _quadratic_regularizer(weights, vars, norm=2):
     """Compute the regularization term.
 
     Args:

e95f6259693ab6b82a9d6db93c3e6f31f249656d
qijia.zhou <qijia.zhou@jingchi.ai>
2018-04-08 19:37:48 -0700

    add stuff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CL
 Continual Learning Library
+
+Please make sure that your keras version is 2.0.5
--- models/model_SI_wrapper.py
+++ models/model_SI_wrapper.py
@@ -5,6 +5,7 @@ from keras.layers import Dense, Reshape
 from keras.optimizers import Adam
 from SI_original.pathint.optimizers import KOOptimizer, PATH_INT_PROTOCOL
 
+
 class SIModel:
   def __init__(self, *args, **kwargs):
     self.keras_loss = kwargs.get('keras_loss', 'sgd')
@@ -22,13 +23,19 @@ class SIModel:
     self.model.add(Dense(24, activation='relu'))
     self.model.add(Dense(self.output_shape, activation='softmax'))
     protocol_name, protocol = PATH_INT_PROTOCOL(omega_decay='sum', xi=1e-3 )
-    opt = KOOptimizer(Adam(lr=self.learning_rate, beta_1=0.9, beta_2=0.999),
+    self.opt = KOOptimizer(Adam(lr=self.learning_rate, beta_1=0.9, beta_2=0.999),
                       model=self.model, **protocol)
-    self.model.compile(loss='categorical_crossentropy', optimizer=opt,
+    self.model.compile(loss='categorical_crossentropy',
+                       optimizer=self.opt,
                        metrics=['accuracy'])
 
   def fit(self, traintype, task_dict, *args, **kwargs):
-    return self.model.fit(*args, **kwargs)
+    history = self.model.fit(*args, **kwargs)
+    if traintype == 'seq':
+      self.opt.update_task_metrics(kwargs['x'], kwargs['y'], kwargs['batch_size'])
+      self.opt.update_task_vars()
+    return history
+
 
   def evaluate(self, *args, **kwargs):
     return self.model.evaluate(x=kwargs['x'], y=kwargs['y'], verbose=0)
